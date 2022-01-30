55bd6942e62812506b117ece47706ec63e6083d3
Gavle-hub <erikdaniel650@gmail.com>
2022-01-30 01:02:54 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./style.css"
+import { useState } from "react";
 function App() {
+  const [getFirstText, setFirstText] = useState("");
+  const [getSecondText, setSecondText] = useState("");
+  const [getFinalText, setFinalText] = useState("");
+
+  function compare(e) {
+    let firstText = getFirstText;
+    let secondText = e.toString();
+    console.log(firstText.length);
+
+    if(firstText.length > 0 && secondText.length > 0){
+        for(let i = 0; i < secondText.length; i++){
+            console.log("second letter = " + secondText.charAt(i));
+            console.log("first letter = " + firstText.charAt(i));
+
+            if(secondText.charAt(i) !== firstText.charAt(i)){
+                let startPosition = i; //gives the letter back
+                let finalText = secondText.substring(0,i) + "<span class='different'>" + secondText.substring(i,i + 1) + "</span>";
+                setFinalText(finalText);
+            }
+        }
+    }
+    else {
+      console.log(false)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Text Compare</h1>
+      <div className='container'>
+        <div contentEditable="true" className='text' onInput={(e) => setFirstText(e.target.innerHTML)}></div>
+        <div contentEditable="true" className='text' onInput={(e) => {setSecondText(e.target.innerHTML);compare(e.target.innerHTML)}}></div>
+      </div>
     </div>
   );
 }
