9347f3a088389d17fc8d53b7bc4bc866b13031a9
Gavle-hub <erikdaniel650@gmail.com>
2022-01-31 18:18:20 -0800

    solved the highlighting wrong word

--- src/App.js
+++ src/App.js
@@ -5,35 +5,55 @@ function App() {
   const [getSecondText, setSecondText] = useState("");
   const [getFinalText, setFinalText] = useState("");
 
-  function compare(e) {
-    let firstText = getFirstText;
-    let secondText = e.toString();
-    console.log(firstText.length);
-
-    if(firstText.length > 0 && secondText.length > 0){
-        for(let i = 0; i < secondText.length; i++){
-            console.log("second letter = " + secondText.charAt(i));
-            console.log("first letter = " + firstText.charAt(i));
-
-            if(secondText.charAt(i) !== firstText.charAt(i)){
-                let startPosition = i; //gives the letter back
-                let finalText = secondText.substring(0,i) + "<span class='different'>" + secondText.substring(i,i + 1) + "</span>";
-                setFinalText(finalText);
-            }
-        }
-    }
-    else {
-      console.log(false)
-    }
+  function handleScrollTop(e) {
+    console.log(e.target)
+    let back = document.getElementsByClassName("back");
+    let scroll = e.target.scrollTop;
+    
+    back.scrollTop = scroll;
   }
 
+
+
+  function handleInput(text) {
+    let highlight = document.getElementById("highlight");
+    let firstText = getFirstText;
+    let secondText = text.target.value;
+
+    if(highlight.innerHTML === ""){
+      highlight.innerHTML = secondText;
+    }
+
+      let finalText = "";
+      for(let i = 0; i < secondText.length; i++){
+          if(secondText.charAt(i) !== firstText.charAt(i)){
+            finalText =  finalText + "<mark>" + secondText.charAt(i) + "</mark>";
+          }
+          else {
+            finalText = finalText + secondText.charAt(i)
+          }
+        }
+        
+        highlight.innerHTML = finalText
+      
+       
+
+  }
   return (
     <div className="App">
       <h1>Text Compare</h1>
       <div className='container'>
-        <div contentEditable="true" className='text' onInput={(e) => setFirstText(e.target.innerHTML)}></div>
-        <div contentEditable="true" className='text' onInput={(e) => {setSecondText(e.target.innerHTML);compare(e.target.innerHTML)}}></div>
+        <div className='subContainer'>
+          <div className='back'>
+            <div id='highlight' className="highlights">
+              {/* {getFinalText} */}
+            </div>
+          </div>
+          <textarea className='secondTextArea' onChange={(e) => handleInput(e)} onScroll={(e) => handleScrollTop(e)}></textarea>
+        </div>
+        <textarea className='firstTextArea' onChange={(e) => {setFirstText(e.target.value)}} onScroll={(e) => handleScrollTop(e)}></textarea>
       </div>
+
     </div>
   );
 }
